40b5dab15dea52fc2f861516950487c7cd0d6d19
Juan LB <ljuan7397@gmail.com>
2023-11-22 18:48:47 +0100

    Add RetrieveAreaUseCase and update AreaController

---
+++ src/modules/area/application/UseCases/RetrieveArea.useCase.ts
@@ -0,0 +1,25 @@
+import { ID, UseCase } from '@common';
+import { AreaDTO, AreaMapper } from '../../Area.mapper';
+import { AreaPostgresRepository } from '../../infrastructure/Area.postgre.repository';
+import { Inject } from '@nestjs/common';
+import { AreaRepository } from '../../domain/Area.repository.port';
+
+export interface RetrieveAreaDTO {
+  id: string;
+}
+
+export class RetrieveAreaUseCase implements UseCase<RetrieveAreaDTO, AreaDTO> {
+  constructor(
+    @Inject(AreaRepository)
+    private readonly areaPostgresRepository: AreaPostgresRepository,
+  ) {}
+  async run(retrieveAreaDTO: RetrieveAreaDTO): Promise<AreaDTO> {
+    try {
+      const id = new ID(retrieveAreaDTO.id);
+      const area = await this.areaPostgresRepository.findOneById(id);
+      return AreaMapper.toDTO(area);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
--- src/modules/area/http/Area.controller.ts
+++ src/modules/area/http/Area.controller.ts
@@ -1,1 +1,34 @@
-export class AreaController {}
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AreaPostgresRepository } from '../infrastructure/Area.postgre.repository';
+import {
+  RetrieveAreaDTO,
+  RetrieveAreaUseCase,
+} from '../application/UseCases/RetrieveArea.useCase';
+import { AreaDTO } from '../Area.mapper';
+
+@ApiTags('area')
+@Controller('area')
+export class AreaController {
+  constructor(
+    @Inject(AreaPostgresRepository)
+    private readonly areaPostgresRepository: AreaPostgresRepository,
+  ) {}
+
+  @Post()
+  async createArea(): Promise<void> {
+    // const createArea = new CreateArea(this.areaPostgresRepository);
+    // const areaCreated = await createArea.run();
+    // return AreaMapper.toDTO(areaCreated);
+  }
+
+  @Get('/:id')
+  async getAreaById(
+    @Body() retrieveAreaDTO: RetrieveAreaDTO,
+  ): Promise<AreaDTO> {
+    const retrieveArea = new RetrieveAreaUseCase(this.areaPostgresRepository);
+    const area = await retrieveArea.run(retrieveAreaDTO);
+
+    return area;
+  }
+}
