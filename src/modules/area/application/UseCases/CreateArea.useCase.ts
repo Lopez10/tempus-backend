513c6101cc89fc18f136aeac774d8386ded618bc
Juan LB <ljuan7397@gmail.com>
2023-11-23 17:27:50 +0100

    Add CreateArea use case implementation

---
+++ src/modules/area/application/UseCases/CreateArea.useCase.ts
@@ -0,0 +1,35 @@
+import { UseCase, Name, ID } from '@common';
+import { Injectable, Inject } from '@nestjs/common';
+import { AreaProps, Area } from '../../domain/Area.entity';
+import {
+  AreaRepository,
+  AreaRepositoryPort,
+} from '../../domain/Area.repository.port';
+
+export interface CreateAreaDTO {
+  name: string;
+  maxCapacity: number;
+  hoursPerReservation: number;
+  restaurantId: string;
+}
+
+@Injectable()
+export class CreateArea implements UseCase<CreateAreaDTO, Area> {
+  constructor(
+    @Inject(AreaRepository)
+    private readonly repository: AreaRepositoryPort,
+  ) {}
+
+  async run(areaDTO: CreateAreaDTO): Promise<Area> {
+    const areaProps: AreaProps = {
+      name: new Name(areaDTO.name),
+      maxCapacity: areaDTO.maxCapacity,
+      hoursPerReservation: areaDTO.hoursPerReservation,
+      restaurantId: new ID(areaDTO.restaurantId),
+    };
+    const area = Area.create(areaProps);
+    await this.repository.insert(area);
+
+    return area;
+  }
+}
