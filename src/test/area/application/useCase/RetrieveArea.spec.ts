a304bc90230e41896cda7657e4f5d277f712d417
Juan LB <ljuan7397@gmail.com>
2023-12-05 21:33:47 +0100

    Add test case for retrieving non-existing area

--- src/test/area/application/useCase/RetrieveArea.spec.ts
+++ src/test/area/application/useCase/RetrieveArea.spec.ts
@@ -5,9 +5,9 @@ import { AreaRepositoryPort } from '../../../../modules/area/domain/Area.reposit
 
 describe('Retrieve Area Use Case', () => {
   it(`
-        GIVEN an existing area
-        WHEN the area is retrieved by id
-        THEN the area should be returned
+      GIVEN an existing area
+      WHEN the area is retrieved by id
+      THEN the area should be returned
     `, async () => {
     const areaReposistory: AreaRepositoryPort = new MockAreaRepository();
     mockAreaData(areaReposistory);
@@ -23,5 +23,26 @@ describe('Retrieve Area Use Case', () => {
 
     // THEN
     expect(areaRetrieved.id).toEqual('Area_1');
+  });
+
+  it(`
+      GIVEN an non existing area
+      WHEN the area is retrieved by id
+      THEN an error should be thrown
+  `, async () => {
+    const areaReposistory: AreaRepositoryPort = new MockAreaRepository();
+    mockAreaData(areaReposistory);
+    const action = new RetrieveAreaUseCase(areaReposistory);
+
+    // GIVEN
+    const areaRequestData = {
+      id: 'Area_10',
+    };
+
+    // WHEN
+    const areaRetrieved = action.run(areaRequestData);
+
+    // THEN
+    await expect(areaRetrieved).rejects.toThrowError('Error: Area not found');
   });
 });
