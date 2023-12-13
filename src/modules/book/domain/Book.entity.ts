213b7797445581a7f043f47c551d51025c84526d
Juan LB <ljuan7397@gmail.com>
2023-12-13 19:25:24 +0100

    Add Book entity and repository port

---
+++ src/modules/book/domain/Book.entity.ts
@@ -0,0 +1,22 @@
+import { AggregateRoot, ID } from '@common';
+
+export interface BookProps {
+  people: number;
+  dateTime: Date;
+  areaId: string;
+  clientId: string;
+  tableId: string;
+  serviceIds: string[];
+}
+
+export class Book extends AggregateRoot<BookProps> {
+  private constructor(props: BookProps, id?: ID) {
+    super(props, id);
+  }
+
+  static create(props: BookProps, id?: ID): Book {
+    const book = new Book(props, id);
+
+    return book;
+  }
+}
---
+++ src/modules/book/domain/Book.repository.port.ts
@@ -0,0 +1,6 @@
+import { RepositoryPort, Name } from '@common';
+import { Book } from './Book.entity';
+
+export interface BookRepositoryPort extends RepositoryPort<Book> {
+  findByBookName(name: Name): Promise<Book | null>;
+}
