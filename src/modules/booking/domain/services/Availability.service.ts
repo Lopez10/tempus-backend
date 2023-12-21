866b73ee08a927a21acd4349e65d212b40e3b9d7
Juan LB <ljuan7397@gmail.com>
2023-12-21 13:07:22 +0100

    Add initial logic to calculate available hours

--- src/modules/booking/domain/services/Availability.service.ts
+++ src/modules/booking/domain/services/Availability.service.ts
@@ -8,6 +8,17 @@ export interface AvailabilityServiceProps {
 
 export class AvailabilityService {
   calculateAvailableHours(bookings: Booking[]): AvailabilityServiceProps[] {
+    const hoursAndAvailable: { start: DateTime; people: number }[] =
+      bookings.map((booking) => {
+        return {
+          start: booking.getPropsCopy().start,
+          people: booking.getPropsCopy().people,
+        };
+      });
+
+    // TODO: calculate available hours based on the bookings of the day substrating the max capacity of the area
+    // TODO: Add the rest of the time between the start and end times of the area
+
     return [];
   }
 }
