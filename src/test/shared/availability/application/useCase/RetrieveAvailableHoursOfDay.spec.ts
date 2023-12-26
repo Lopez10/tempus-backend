b3ba035d114ba4e42a1531efe734a33a757b11ce
Juan LB <ljuan7397@gmail.com>
2023-12-26 09:45:06 +0100

    Refactor RetrieveAvailableHoursOfDayUseCase tests

--- src/test/shared/availability/application/useCase/RetrieveAvailableHoursOfDay.spec.ts
+++ src/test/shared/availability/application/useCase/RetrieveAvailableHoursOfDay.spec.ts
@@ -10,10 +10,12 @@ import { mockBookingData } from '../../../../booking/mockBookingData';
 
 describe('Retrieve Available Hours Of Day Use Case', () => {
   it(`
-    GIVEN a restaurant data
+    GIVEN an area data of restaurant
+    AND a booking to this area
     WHEN I call to the use case to retrieve the available hours of the day
     THEN the available hours of the day should be retrieved
   `, async () => {
+    // GIVEN
     const areaRepository: AreaRepositoryPort = new MockAreaRepository();
     await mockAreaData(areaRepository);
 
@@ -26,7 +28,6 @@ describe('Retrieve Available Hours Of Day Use Case', () => {
       areaRepository,
     );
 
-    // GIVEN
     const retrieveAvailableHoursOfDayDTO = {
       day: '01/01/2024',
       areaId: 'Area_1',
@@ -53,6 +54,82 @@ describe('Retrieve Available Hours Of Day Use Case', () => {
       {
         hour: '11:30',
         available: 9,
+      },
+      {
+        hour: '12:00',
+        available: 10,
+      },
+      {
+        hour: '12:30',
+        available: 10,
+      },
+      {
+        hour: '13:00',
+        available: 10,
+      },
+      {
+        hour: '13:30',
+        available: 10,
+      },
+      {
+        hour: '14:00',
+        available: 10,
+      },
+      {
+        hour: '14:30',
+        available: 10,
+      },
+      {
+        hour: '15:00',
+        available: 10,
+      },
+    ]);
+  });
+
+  it(`
+    GIVEN an area data of restaurant
+    AND there are not bookings to this area and this day
+    WHEN I call to the use case to retrieve the available hours of the day
+    THEN the available hours of the day should be retrieved
+  `, async () => {
+    // GIVEN
+    const areaRepository: AreaRepositoryPort = new MockAreaRepository();
+    await mockAreaData(areaRepository);
+
+    const bookingRepository: BookingRepositoryPort =
+      new MockBookingRepository();
+
+    const action = new RetrieveAvailableHoursOfDayUseCase(
+      bookingRepository,
+      areaRepository,
+    );
+
+    const retrieveAvailableHoursOfDayDTO = {
+      day: '01/01/2024',
+      areaId: 'Area_1',
+      people: 1,
+    };
+
+    // WHEN
+    const hoursAvailable = await action.run(retrieveAvailableHoursOfDayDTO);
+
+    // THEN
+    expect(hoursAvailable).toEqual([
+      {
+        hour: '10:00',
+        available: 10,
+      },
+      {
+        hour: '10:30',
+        available: 10,
+      },
+      {
+        hour: '11:00',
+        available: 10,
+      },
+      {
+        hour: '11:30',
+        available: 10,
       },
       {
         hour: '12:00',
