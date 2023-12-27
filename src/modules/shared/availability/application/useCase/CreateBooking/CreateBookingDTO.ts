f27bebc48c129493b084bef79c2692b233d37ae8
Juan LB <ljuan7397@gmail.com>
2023-12-27 12:14:25 +0100

    Add CreateBookingUseCase and CreateBookingDTO to shared availability module

---
+++ src/modules/shared/availability/application/useCase/CreateBooking/CreateBookingDTO.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBookingDTO {
+  @ApiProperty()
+  people: number;
+
+  @ApiProperty()
+  start: string;
+
+  @ApiProperty()
+  end: string;
+
+  @ApiProperty()
+  areaId: string;
+
+  @ApiProperty()
+  clientId: string;
+
+  @ApiProperty()
+  tableId: string;
+
+  @ApiProperty()
+  serviceIds: string[];
+}
--- src/modules/shared/availability/application/useCase/index.ts
+++ src/modules/shared/availability/application/useCase/index.ts
@@ -1,3 +1,6 @@
 export * from './RetrieveAvailableHoursOfDay/HoursAvailableDTO';
 export * from './RetrieveAvailableHoursOfDay/RetrieveAvailableHoursOfDay.useCase';
 export * from './RetrieveAvailableHoursOfDay/RetrieveAvailableHoursOfDayDTO';
+
+export * from './CreateBooking/CreateBooking.useCase';
+export * from './CreateBooking/CreateBookingDTO';
