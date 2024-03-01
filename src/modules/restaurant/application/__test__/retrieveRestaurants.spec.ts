b3c3b981318190c9f53ad4491f89d6043f30f894
Juan LB <ljuan7397@gmail.com>
2024-03-01 09:28:52 +0100

    Add restaurant test files and mock repository

--- src/modules/restaurant/application/__test__/retrieveRestaurants.spec.ts
+++ src/modules/restaurant/application/__test__/retrieveRestaurants.spec.ts
@@ -1,63 +1,63 @@
 import {
-  Restaurant,
-  RestaurantRepositoryPort,
-  RetrieveRestaurantsUseCase,
-} from '@modules';
-import { RestaurantMockRepository } from '../../infrastructure/restaurant.mock.repository';
-import { Description, Email, ID, Name, PaginationQueryParams } from '@common';
+	Restaurant,
+	RestaurantRepositoryPort,
+	RetrieveRestaurantsUseCase,
+} from "@modules";
+import { RestaurantMockRepository } from "../../infrastructure/__test__/restaurant.mock.repository";
+import { Description, Email, ID, Name, PaginationQueryParams } from "@common";
 
-describe('Retrieve Restaurants Use Case', () => {
-  it(`
+describe("Retrieve Restaurants Use Case", () => {
+	it(`
         GIVEN there are many restaurants
         WHEN the restaurants are retrieved
         THEN the restaurant should be retrieved 
     `, async () => {
-    const restaurantReposistory: RestaurantRepositoryPort =
-      new RestaurantMockRepository();
-    const action = new RetrieveRestaurantsUseCase(restaurantReposistory);
+		const restaurantReposistory: RestaurantRepositoryPort =
+			new RestaurantMockRepository();
+		const action = new RetrieveRestaurantsUseCase(restaurantReposistory);
 
-    // GIVEN
-    mockRestaurantData(restaurantReposistory);
-    const pagination: PaginationQueryParams = {
-      limit: 10,
-      offset: 0,
-      page: 0,
-      orderBy: { field: 'name', param: 'asc' },
-    };
-    const restaurantRequestData = {
-      criteria: {},
-      pagination,
-    };
+		// GIVEN
+		mockRestaurantData(restaurantReposistory);
+		const pagination: PaginationQueryParams = {
+			limit: 10,
+			offset: 0,
+			page: 0,
+			orderBy: { field: "name", param: "asc" },
+		};
+		const restaurantRequestData = {
+			criteria: {},
+			pagination,
+		};
 
-    // WHEN
-    const restaurantsRetrieved = await action.run(restaurantRequestData);
+		// WHEN
+		const restaurantsRetrieved = await action.run(restaurantRequestData);
 
-    // THEN
-    expect(restaurantsRetrieved.length).toEqual(2);
-  });
+		// THEN
+		expect(restaurantsRetrieved.length).toEqual(2);
+	});
 });
 
 export async function mockRestaurantData(
-  restaurantRepository: RestaurantRepositoryPort,
+	restaurantRepository: RestaurantRepositoryPort,
 ) {
-  return await restaurantRepository.insertSome([
-    Restaurant.create(
-      {
-        name: new Name('Restaurant_1'),
-        description: new Description('Restaurant_1'),
-        email: new Email('restaurant1@gmail.com'),
-        capacity: 10,
-      },
-      new ID('Restaurant_1'),
-    ),
-    Restaurant.create(
-      {
-        name: new Name('Restaurant_2'),
-        description: new Description('Restaurant_2'),
-        email: new Email('restaurant2@gmail.com'),
-        capacity: 10,
-      },
-      new ID('Restaurant_2'),
-    ),
-  ]);
+	return await restaurantRepository.insertSome([
+		Restaurant.create(
+			{
+				name: new Name("Restaurant_1"),
+				description: new Description("Restaurant_1"),
+				email: new Email("restaurant1@gmail.com"),
+				capacity: 10,
+			},
+			new ID("Restaurant_1"),
+		),
+		Restaurant.create(
+			{
+				name: new Name("Restaurant_2"),
+				description: new Description("Restaurant_2"),
+				email: new Email("restaurant2@gmail.com"),
+				capacity: 10,
+			},
+			new ID("Restaurant_2"),
+		),
+	]);
 }
