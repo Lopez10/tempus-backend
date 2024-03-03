76b71360cd3ed8cbedf6ffa31085e54f772a7e19
Juan LB <ljuan7397@gmail.com>
2024-03-03 11:24:45 +0100

    restart cache

--- src/modules/availability/application/useCase/RetrieveAvailableDays/AvailableDays.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class AvailableDaysDto {
-  @ApiProperty({
-    example: ['01/02/2023', '02/02/2023', '04/02/2023', '05/02/2023'],
-    description: 'Date of the availability',
-  })
-  days: string[];
-}
