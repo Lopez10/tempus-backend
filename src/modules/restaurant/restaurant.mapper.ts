32fa54595420aec9d6384bc0936761cd35e098c3
Juan LB <ljuan7397@gmail.com>
2024-03-03 11:29:02 +0100

    fix name

--- src/modules/restaurant/restaurant.mapper.ts
+++ src/modules/restaurant/restaurant.mapper.ts
@@ -2,32 +2,32 @@ import { Description, Email, ID, Name } from '@common';
 import { Restaurant } from './domain';
 
 export interface RestaurantDto {
-  id: string;
-  name: string;
-  email: string;
-  description: string;
-  capacity: number;
+	id: string;
+	name: string;
+	email: string;
+	description: string;
+	capacity: number;
 }
 export class RestaurantMapper {
-  static toDomain(restaurantDto: RestaurantDto): Restaurant {
-    return Restaurant.create(
-      {
-        name: new Name(restaurantDto.name),
-        email: new Email(restaurantDto.email),
-        description: new Description(restaurantDto.description),
-        capacity: restaurantDto.capacity,
-      },
-      new ID(restaurantDto.id),
-    );
-  }
+	static toDomain(restaurantDto: RestaurantDto): Restaurant {
+		return Restaurant.create(
+			{
+				name: new Name(restaurantDto.name),
+				email: new Email(restaurantDto.email),
+				description: new Description(restaurantDto.description),
+				capacity: restaurantDto.capacity,
+			},
+			new ID(restaurantDto.id),
+		);
+	}
 
-  static toDTO(restaurant: Restaurant): RestaurantDto {
-    return {
-      id: restaurant.propsCopy.id.value,
-      name: restaurant.propsCopy.name.value,
-      email: restaurant.propsCopy.email.value,
-      description: restaurant.propsCopy.description.value,
-      capacity: restaurant.propsCopy.capacity,
-    };
-  }
+	static toDTO(restaurant: Restaurant): RestaurantDto {
+		return {
+			id: restaurant.propsCopy.id.value,
+			name: restaurant.propsCopy.name.value,
+			email: restaurant.propsCopy.email.value,
+			description: restaurant.propsCopy.description.value,
+			capacity: restaurant.propsCopy.capacity,
+		};
+	}
 }
