601921d9db1e4c9cd4e0661ef50e7c83318d3e04
Juan LB <ljuan7397@gmail.com>
2024-03-03 11:31:55 +0100

    rename files

--- src/modules/booking/application/useCase/RetrieveBooking/RetrieveBooking.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class RetrieveBookingDto {
-  @ApiProperty({
-    example: '1',
-    description: 'Id of the booking',
-  })
-  id: string;
-}
--- src/modules/booking/domain/Booking.repository.port.ts
+++
@@ -1,9 +0,0 @@
-import { RepositoryPort, ID, DateVO } from '@common';
-import { Booking } from './booking.entity';
-
-export interface BookingRepositoryPort extends RepositoryPort<Booking> {
-	findByDayAndAreaId(day: DateVO, areaId: ID): Promise<Booking[]>;
-	findByMonthAndAreaId(month: DateVO, areaId: ID): Promise<Booking[]>;
-}
-
-export const BookingRepository = Symbol('BookRepository');
